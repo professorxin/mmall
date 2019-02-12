c234a672aa6a70ca0efe3f98f47a8a9f3b57c3f2
professorxin <1252778220@qq.com>
2019-02-12 15:40:51 +0800

    cookie_domain

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    private final static String COOKIE_DOMAIN = ".happymmall.com";
+//    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_DOMAIN = ".lzxin.top";
     private final static String COOKIE_NAME = "mmall_login_token";
 
     public static String readLoginToken(HttpServletRequest request) {
